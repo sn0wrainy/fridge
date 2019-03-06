91d530b229318ac00a8b965415488ef8513f0e6f
Karolina Kowasz <karolinamosur@gmail.com>
2019-03-06 12:52:15 +0100

    Filtering by category in Recipes list view

--- api/views.py
+++ api/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from api.serializers import RecipeSerializer, CategorySerializer, IngredientSerializer
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 from api.models import Recipe, Category, Ingredient
 
 class RecipesView(generics.ListAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("category",)
 
 class RecipesItemView(generics.RetrieveAPIView):
     queryset = Recipe.objects.all()
@@ -18,3 +21,4 @@ class CategoriesView(generics.ListAPIView):
 class IngredientsView(generics.ListAPIView):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+
