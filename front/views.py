939b43cf11834ffdb365ed71ebbe23481be9ed2f
Karolina Kowasz <karolinamosur@gmail.com>
2019-03-12 12:16:39 +0100

    Ingredients checkboxes

--- front/views.py
+++ front/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from api.models import Ingredient
 
 
 class IndexView(View):
 
     def get(self, request):
-        return render(request, "index.html")
+        ingredients = Ingredient.objects.all()
+        return render(request, "index.html", {"ingredients": ingredients})
